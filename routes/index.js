f0938d37cb9f542a16c3676995975e22f6786dd1
lukendev <lucas.azevedo2104@gmail.com>
2020-05-14 15:27:49 -0300

    pagina quem-somos com modo responsivo funcionando

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,14 @@ router.get('/', function(req, res) {
   res.render('index');
 });
 
+router.get('/nossos-servicos', function(req, res) {
+  res.render('nossos-servicos');
+})
+
+router.get('/quem-somos', function(req, res) {
+  res.render('quem-somos');
+})
+
 router.get('/teste', function(req, res) {
   res.render('template')
 });
