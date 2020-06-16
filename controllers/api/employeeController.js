88b973bdbc129bc3523bbd2c24c3d6e7edd8ff1b
luizrobertoao <luizrobertoao29@gmail.com>
2020-06-16 17:32:17 -0300

    feat: Registro das alterações em funcionários.
    Co-authored-by: lucaspuglielli <lucaspuglielli@gmail.com>
    Co-authored-by: lukendev <lucas.azevedo2104@gmail.com>

--- controllers/api/employeeController.js
+++ controllers/api/employeeController.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize')
-const { Employee } = require('../../models')
+const { Employee, Working_day} = require('../../models')
 
 
 const employeeController = {
@@ -15,7 +15,72 @@ const employeeController = {
             })
         }
         
+    },
+    update: async (req, res) => {
+        const {
+            editemployeenameselection,
+            editemployeename,
+            editemployeelastname,
+            editemployeeemail,
+            editemployeephone,
+            editemployeeshiftstart,
+            editemployeeshiftend,
+            dominfosfuncionario,
+            seginfosfuncionario,
+            terinfosfuncionario,
+            quainfosfuncionario,
+            quiinfosfuncionario,
+            sexinfosfuncionario,
+            sabinfosfuncionario,
+        } = req.body;
+        
+        function verificaDia(dia){
+            if(dia == "on"){
+                return 1;
+            } else{
+                return 0;
+            }
+        }
+
+        try {
+            const employee = await Employee.update(
+                {
+                name: editemployeename,
+                lastname: editemployeelastname,
+                email: editemployeeemail,
+                phone: editemployeephone,
+                shiftstart: editemployeeshiftstart,
+                shiftend: editemployeeshiftend,
+                updatedAt: new Date(), 
+            },
+            {
+                where: {id: editemployeenameselection}
+            }
+            )
+            const workingDay = await Working_day.update(
+                {
+                    sunday: verificaDia(dominfosfuncionario),
+                    monday: verificaDia(seginfosfuncionario),
+                    tuesday: verificaDia(terinfosfuncionario),
+                    wednesday: verificaDia(quainfosfuncionario),
+                    thursday: verificaDia(quiinfosfuncionario),
+                    friday: verificaDia(sexinfosfuncionario),
+                    saturday: verificaDia(sabinfosfuncionario),
+                },
+                {
+                    where: {id_employee: editemployeenameselection}
+                }
+            )
+            return res.redirect('/administracao')
+        } catch(e){
+            return res.status(400).json({
+              error: true,
+              msg: 'Erro na requisição, tente novamente'
+            }
+            )}
     }
+
 }
+
 
 module.exports = employeeController;
--- routes/api.js
+++ routes/api.js
@@ -5,7 +5,9 @@ const router = express.Router();
 const employeeController = require('../controllers/api/employeeController');
 const workingDayController = require('../controllers/api/workingDaysController');
 
+// Funcionários
 router.get('/funcionario', employeeController.index);
 router.get('/dias', workingDayController.index);
+router.put('/employee-edit', employeeController.update)
 
 module.exports = router;
