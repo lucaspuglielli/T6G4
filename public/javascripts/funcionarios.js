c17e6b5b4b6afe4b882d74eeeaa183a177336673
lucaspuglielli <lucaspuglielli@gmail.com>
2020-06-18 16:38:36 -0300

    feat: conteudo dinamico da pagina profissionais
    Co-authored-by: lukendev <lucas.azevedo2104@gmail.com>
    Co-authored-by: luizrobertoao <luizrobertoao29@gmail.com>

---
+++ public/javascripts/funcionarios.js
@@ -0,0 +1,22 @@
+const lista = document.querySelector(".listadefuncionarios");
+
+const employees = []
+
+fetch("http://localhost:3000/api/funcionario")
+	.then((resposta) => resposta.json())
+	.then((dados) => {
+		lista.innerHTML = "";
+		dados.forEach((funcionario) => {
+			lista.innerHTML += `
+            <div class="card card-funcionario" style="margin-bottom: 30px">
+                <div>
+                    <img src="/images/${funcionario.photo}" style="width: 240px;" alt="funcionario">
+                </div>
+                <div class="card-body">
+                    <div class="titulofuncionariohome">${funcionario.name} ${funcionario.lastname}</div>
+                    <div class="subtitulofuncionariohome">Robbie Williams</div>
+                </div>
+            </div>
+            `;
+        });
+	});
