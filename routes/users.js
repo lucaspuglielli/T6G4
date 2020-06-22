9b0a55ac29da3269ccc05afc8c1bc7b3918137dd
lucaspuglielli <lucaspuglielli@gmail.com>
2020-06-22 20:57:43 -0300

    refactor: pequenos ajustes de formatacao
    Co-authored-by: lukendev <lucas.azevedo2104@gmail.com>
    Co-authored-by: luizrobertoao <luizrobertoao29@gmail.com>

--- routes/users.js
+++ routes/users.js
@@ -1,23 +1,23 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const userController = require('../controllers/userController');
-const clientScheduleController = require('../controllers/clientScheduleController');
-const sessionVerifier = require('../middleware/sessionVerifier');
-const auth = require('../middleware/auth');
-const blockAdm = require('../middleware/blockAdmMiddleware')
+const userController = require("../controllers/userController");
+const clientScheduleController = require("../controllers/clientScheduleController");
+const sessionVerifier = require("../middleware/sessionVerifier");
+const auth = require("../middleware/auth");
+const blockAdm = require("../middleware/blockAdmMiddleware");
 
 /* User register routes */
-router.get('/registro', blockAdm, sessionVerifier, userController.create);
-router.post('/registro', userController.store);
+router.get("/registro", blockAdm, sessionVerifier, userController.create);
+router.post("/registro", userController.store);
 
-router.get('/perfil', auth, userController.index);
-router.get('/perfil/agendamento', auth, userController.index2);
-router.put('/editar', auth, userController.update);
+router.get("/perfil", auth, userController.index);
+router.get("/perfil/agendamento", auth, userController.index2);
+router.put("/editar", auth, userController.update);
 
 // Rotas para agendamento de usuário.agendamento
 
-router.get('/agendamento', blockAdm, auth, clientScheduleController.index);
-router.post('/agendamento', clientScheduleController.store);
+router.get("/agendamento", blockAdm, auth, clientScheduleController.index);
+router.post("/agendamento", clientScheduleController.store);
 
 module.exports = router;
