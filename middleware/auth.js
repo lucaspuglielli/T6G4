142230819d8e6646fcf917cfc7c4372db385d62f
luizrobertoao <luizrobertoao29@gmail.com>
2020-06-26 11:55:23 -0300

    fix: Quebra de fluxo do agendamento pelo usuário sem login.
    Co-authored-by: lucaspuglielli <lucaspuglielli@gmail.com>
    Co-authored-by: lukendev <lucas.azevedo2104@gmail.com>

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,8 @@
 module.exports = (req, res, next) => {
-    if(!req.session.user) res.redirect('/login')
+    if(!req.session.user){
+        req.session.previousUrl = req.originalUrl;
+        res.redirect('/login')
+    }
     if(!req.session.user.admin){
         res.locals.user = req.session.user;
         next();
