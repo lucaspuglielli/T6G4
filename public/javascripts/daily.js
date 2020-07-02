088803eb636a6b0f4ead5b722fa71d4973255053
lucaspuglielli <lucaspuglielli@gmail.com>
2020-07-02 16:26:48 -0300

    refactor: alteracao do formato de cards para table dos agendamentos na pagina de administracao
    Co-authored-by: lukendev <lucas.azevedo2104@gmail.com>
    Co-authored-by: luizrobertoao <luizrobertoao29@gmail.com>

--- public/javascripts/daily.js
+++ public/javascripts/daily.js
@@ -5,6 +5,7 @@ let categorias = [];
 let servicos = [];
 let funcionario = [];
 let agendamentos = [];
+let clientes = [];
 
 
 fetch("http://localhost:3000/api/categories")
@@ -25,6 +26,12 @@ fetch("http://localhost:3000/api/funcionario")
         funcionario = dados;
     });
 
+fetch("http://localhost:3000/api/funcionario")
+    .then((resposta) => resposta.json())
+    .then((dados) => {
+        clientes = dados;
+    });
+
 
 fetch("http://localhost:3000/api/schedules")
     .then((resposta) => resposta.json())
@@ -33,61 +40,40 @@ fetch("http://localhost:3000/api/schedules")
         lista.innerHTML = "";
         dados.forEach((agendamento) => {
             lista.innerHTML += `
-            <form class="forms" id="formdeletar" method="POST" action="/administracao/daily?_method=delete">
-                <input id="idschedule" name="idschedule" value="${agendamento.id}" type="text" hidden/>
-                <div class="cartao-servico mb-5 p-4">
-                    <div class="d-flex">
-                        <div>
-                            <img class="img-servico" src="/images/${categorias[servicos[agendamento.id_service - 1].id_category - 1].icon}" alt="${categorias[servicos[agendamento.id_service - 1].id_category - 1].name}">
-                        </div>
-                        <div class="d-flex flex-column justify-content-between">
-                            <div>
-                                <h2 class="titulo-servico-card">${servicos[agendamento.id_service - 1].name}</h2>
-                            </div>
-                            <div>
-                                <p class="data-servico-card">${agendamento.start_date} - ${agendamento.start_time}</p>
-                            </div>
-                            <div class="d-flex">
-                                <span class="label-info">Profissional: </span>
-                                <span class="nome-info">${funcionario[agendamento.id_employee - 1].name} ${funcionario[agendamento.id_employee - 1].lastname}</span>
-                                
-                            </div>
-                            <div class="d-flex">
-                                <span class="label-info">Valor: </span>
-                                <span class="nome-info">R$:${servicos[agendamento.id_service - 1].price}</span>
-                            </div>
-                            <div class="d-flex">
-                                <span class="label-info">Status: </span>
-                                <span class="status-info">Agendado</span>
-                            </div>
-                        </div>
-                        <div class="d-flex align-items-start">
-                
-                <button type="button" class="d-flex align-items-center btn btn-danger" data-toggle="modal" data-target="#exampleModal${agendamento.id}">
-                Cancelar<i class="fa fa-close" style="font-size: 3.5vh; color:white; margin-left: 10px;"></i>
-                </button>
-
-                
-                <div class="modal fade" id="exampleModal${agendamento.id}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Tem certeza que deseja cancelar esse agendamento?</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
+            <tr>
+                <td>${clientes[agendamento.id_client - 1].name} ${clientes[agendamento.id_client - 1].lastname}</td>
+                <td>${agendamento.start_date}</td>
+                <td>${agendamento.start_time}</td>
+                <td>${servicos[agendamento.id_service - 1].name}</td>
+                <td>${funcionario[agendamento.id_employee - 1].name} ${funcionario[agendamento.id_employee - 1].lastname}</td>
+                <td>${servicos[agendamento.id_service - 1].price}</td>
+                <td>Agendado</td>
+                <td>
+                    <form id="formdeletar" method="POST" action="/administracao/daily?_method=delete">
+                        <input id="idschedule" name="idschedule" value="${agendamento.id}" type="text" hidden/>
+                        <button type="button" class="d-flex align-items-center btn btn-danger" data-toggle="modal" data-target="#exampleModal${agendamento.id}">
+                            Cancelar
+                            <i class="fa fa-close" style="font-size: 3.5vh; color:white; margin-left: 10px;"></i>
                         </button>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="submit" class="btn btn-primary">Sim</button>
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Não</button>
-                    </div>
-                    </div>
-                </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</form>
+                        <div class="modal fade" id="exampleModal${agendamento.id}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Tem certeza que deseja cancelar esse agendamento?</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="submit" class="btn btn-primary">Sim</button>
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Não</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </td>
+            </tr>
             `;
         });
 
