04aceafb99bddd3f3d1869672ccb901d5a852d56
lucaspuglielli <lucaspuglielli@gmail.com>
2020-07-03 11:07:27 -0300

    teste de configuracao do db no heroku

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
-    username: "root",
-    password: null,
-    database: "casaraodb",
-    host: "127.0.0.1",
-    dialect: "mysql",
+    username: "morgkekcloinnz",
+    password: "3d378e02fdd9402c97d716a32949c14c35c817091497b5717f3f91321abf04c3",
+    database: "d8mkpmuds5csap",
+    host: "ec2-52-20-248-222.compute-1.amazonaws.com",
+    dialect: "postgres",
   };
