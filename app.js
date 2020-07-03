6b6e901b2cc130418d742d0b699afd0cc1378081
lucaspuglielli <lucaspuglielli@gmail.com>
2020-07-03 11:28:11 -0300

    alteracao de porta do db

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const session = require('express-session');
 const methodOverride = require('method-override')
+const dotenv = require('dotenv').config()
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
--- config/database.js
+++ config/database.js
@@ -1,7 +1,10 @@
+
+
 module.exports = {
     username: "morgkekcloinnz",
     password: "3d378e02fdd9402c97d716a32949c14c35c817091497b5717f3f91321abf04c3",
     database: "d8mkpmuds5csap",
     host: "ec2-52-20-248-222.compute-1.amazonaws.com",
     dialect: "postgres",
+    port: 5432
   };
